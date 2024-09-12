bb1d97487b44ad7fa1d4922bac0e35e486403307
Harold Navarro <kaendor50@gmail.com>
2024-09-11 23:38:32 -0600

    Exercise 01 number variable

---
+++ Learn_String_Manipulation_By_Building_A_Cipher/Exercise_01.py
@@ -0,0 +1,8 @@
+# Step 1
+# Variables are an essential part of Python and any programming language. A variable is a name that references or points to an object. You can declare a variable by writing the variable name on the left side of the assignment operator = and specifying the value to assign to that variable on the right side of the assignment operator:
+
+# Example Code
+# variable_name = value
+# Create a variable called number and assign the value 5 to your new variable.
+
+number = 5
