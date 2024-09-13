311b881eaf09c7b4ee5577b2fca91af06d861953
Harold Navarro <kaendor50@gmail.com>
2024-09-13 11:46:38 -0600

    access a value of alphabet at index position

--- Learn_String_Manipulation_By_Building_A_Cipher/Exercise_19.py
+++ Learn_String_Manipulation_By_Building_A_Cipher/Exercise_19.py
@@ -1,6 +1,7 @@
 # Step 19
 # Declare a new variable named shifted. Use the bracket notation to access the value of alphabet at index index and assign it to your new variable.
 
+
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
