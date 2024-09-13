a681cea1c132af6d6dc8b20442e2ee8226a574b7
Harold Navarro <kaendor50@gmail.com>
2024-09-13 12:13:19 -0600

    change to meaningful name at iteration variable

---
+++ Learn_String_Manipulation_By_Building_A_Cipher/Exercise_25.py
@@ -0,0 +1,11 @@
+# Step 25
+# The iteration variable can have any valid name, but it's convenient to give it a meaningful name.
+
+# Rename your i variable to char.
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text:
+    print(char)
