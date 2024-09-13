6fa0ac51c6301bf7ba012325461eb000349b711f
Harold Navarro <kaendor50@gmail.com>
2024-09-12 20:04:25 -0600

    Add new number variable

---
+++ Learn_String_Manipulation_By_Building_A_Cipher/Exercise_09.py
@@ -0,0 +1,9 @@
+# Step 9
+# As you can see, the output of printing type(text) is <class 'str'>, which means that your variable is a string, indicated as str.
+
+# Now go to a new line and create another variable called shift and assign the value 3 to this variable.
+
+text = 'Hello World'
+print(type(text))
+
+shift = 3
