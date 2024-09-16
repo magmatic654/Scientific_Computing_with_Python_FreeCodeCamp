cccc0d8ae810720667587f307130aad9a3acf7a1
Harold Navarro <kaendor50@gmail.com>
2024-09-15 22:27:41 -0600

    assign the current value of encrypted_text plus the shifted alphabet element

---
+++ Learn_String_Manipulation_By_Building_A_Cipher/Exercise_37.py
@@ -0,0 +1,14 @@
+# Step 37
+# Now, replace new_char with encrypted_text. Also, modify the print() call into print('char:', char, 'encrypted text:', encrypted_text) to reflect this change
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    index = alphabet.find(char)    
+    new_index = index + shift
+    encrypted_text = alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
+
