e117532c97699a0865b989cceb0f3979705b6f31
Harold Navarro <kaendor50@gmail.com>
2024-09-20 21:21:25 -0600

    modifying the print does put outside the for loop for print once time

---
+++ Learn_String_Manipulation_By_Building_A_Cipher/Exercise_47.py
@@ -0,0 +1,17 @@
+# Step 47
+# Next, modify your print() call to print 'encrypted text:', encrypted_text and put it outside the for loop, so that the encrypted string is printed one time.
+
+text = 'Hello Zaira'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = (index + shift) % len(alphabet)
+        encrypted_text += alphabet[new_index]
+    print('char:', char)
+print('encrypted text:', encrypted_text)
